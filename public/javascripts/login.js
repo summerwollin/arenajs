aa1d77f2c694be0e2c1488eebc68bf71e72773bc
Summer Wollin <summer.wollin@gmail.com>
2016-06-26 22:29:51 -0600

    style main view

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,13 +1,13 @@
-$(function() {
-
-  $("#submit-username").on("click", function() {
-    console.log($(".username-input").val());
-    $.post("/login", { username: $(".username-input").val()}, function(response) {
-        console.log('res: ', response);
-        window.localStorage.setItem('token', response.token);
-        window.location.href = '/chat.html';
-      })
-    })
-
-    
-});
+// $(function() {
+//
+//   $("#submit-username").on("click", function() {
+//     console.log($(".username-input").val());
+//     $.post("/login", { username: $(".username-input").val()}, function(response) {
+//         console.log('res: ', response);
+//         window.localStorage.setItem('token', response.token);
+//         window.location.href = '/chat.html';
+//       })
+//     })
+//
+//
+// });
