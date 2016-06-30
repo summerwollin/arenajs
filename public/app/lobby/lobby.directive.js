906550a266b24c43cff9d620ad0ce95ae37d2a35
Summer Wollin <summer.wollin@gmail.com>
2016-06-29 19:03:41 -0600

    when user leaves game page, hostedgame is deleted

--- public/app/lobby/lobby.directive.js
+++ public/app/lobby/lobby.directive.js
@@ -22,6 +22,7 @@
     var vm = this;
     vm.startHosting = startHosting;
     vm.hostedGames = [];
+    vm.joinGame = joinGame;
     activate();
 
     function activate() {
@@ -32,6 +33,10 @@
     function startHosting(gameOption) {
       console.log('start hosting: ', gameOption);
       socketService.newGameHost(gameOption);
+    }
+
+    function joinGame(game) {
+      console.log('joinGame: ', game);
     }
 
     $scope.$on('sending-hostedGames', function (event, data) {
