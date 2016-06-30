4b692ff93287e726016902a634113b3f5b32e9f0
Summer Wollin <summer.wollin@gmail.com>
2016-06-30 15:31:20 -0600

    prepare to deploy

--- public/app/login/login.directive.js
+++ public/app/login/login.directive.js
@@ -29,7 +29,7 @@
     });
 
     function login() {
-      return $http.post('http://localhost:3000/login', {username: vm.form.login.username})
+      return $http.post('https://arenajs.herokuapp.com/login', {username: vm.form.login.username})
       .then(function (response) {
         console.log('set token');
         socketService.setToken(response.data.token);
