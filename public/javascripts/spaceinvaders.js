badb662df4517d8e426c9d620c2ba3389121cd4c
Summer Wollin <summer.wollin@gmail.com>
2016-06-29 20:45:39 -0600

    remove unreferenced state code from spaceinvaders.js

--- public/javascripts/spaceinvaders.js
+++ public/javascripts/spaceinvaders.js
@@ -138,27 +138,6 @@ function GameLoop(game) {
     }
 }
 
-Game.prototype.pushState = function(state) {
-    //  If there's an enter function for the new state, call it.
-    if (state.enter) {
-        state.enter(game);
-    }
-    //  Set the current state.
-    this.stateStack.push(state);
-};
-
-Game.prototype.popState = function() {
-
-    //  Leave and pop the state.
-    if (this.currentState()) {
-        if (this.currentState().leave) {
-            this.currentState().leave(game);
-        }
-
-        //  Set the current state.
-        this.stateStack.pop();
-    }
-};
 
 //  The stop function stops the game.
 Game.prototype.stop = function Stop() {
