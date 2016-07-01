66461ec1565c9614bf4d05b2de52fdd082362379
Summer Wollin <summer.wollin@gmail.com>
2016-06-30 19:39:57 -0600

    create host factory

---
+++ public/app/services/host.factory.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular.module('arenaApp')
+    .factory('hostService', factory);
+
+
+  factory.$inject = [];
+
+  function factory () {
+
+    return {};
+
+
+
+  }
+}());
